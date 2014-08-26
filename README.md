1a4415d5f15a5ca8781173fae81835dd53f3da25
Dan Flettre <fletd01@yahoo.com>
2014-08-26 03:19:04 -0500

    make it so

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 trippyshift
 ===========
+
+This module takes a buffer holding an image and uses graphicsmagick to overlay an offset second copy of the image while applying the "Add" composite operator.
+
+install:
+```
+npm install trippyshift --save
+```
+
+usage:
+```
+var fs = require('fs'),
+    trippyshift = require('trippyshift');
+
+
+var wooo;
+fs.readFile('./file.jpg', function (err, data) {
+    wooo = data;
+    trippyshift({}, data, writeToDestination);
+});
+
+function writeToDestination(err, trippedOutBuf) {
+    if (err) throw err;
+    fs.writeFileSync('./output.jpg', trippedOutBuf);
+};
+```
+
+an `options` object with `shiftXMaxPercent` and/or shiftYMaxPercent can be passed in optionally as the first argument.
---
+++ index.js
@@ -0,0 +1,64 @@
+var gm = require('gm'),
+  temp = require('temp'),
+  fs = require('fs');
+
+function randomIntInc(low, high) {
+  return Math.floor(Math.random() * (high - low + 1) + low);
+}
+
+module.exports = function (options, buffer, callback) {
+
+  if (!callback && typeof buffer === 'function') {
+    callback = buffer;
+    buffer = options;
+    options = {};
+  }
+
+  var shiftXMaxPercent = options.shiftXMaxPercent || 1,
+    shiftYMaxPercent = options.shiftYMaxPercent || 1
+
+  gm(buffer).identify(function (err, data) {
+    if (err) {
+      callback(err);
+    }
+
+    var imgWidth = data.size.width,
+      imgHeight = data.size.height,
+      shiftX = randomIntInc(0, imgHeight * shiftXMaxPercent),
+      shiftY = randomIntInc(0, imgWidth * shiftYMaxPercent),
+      tempImgRoll = temp.path({
+        suffix: '.' + data.format
+      }),
+      tempImg = temp.path({
+        suffix: '.' + data.format
+      });;
+
+    //TODO: uhh, how about some async
+    gm(buffer)
+      .write(tempImg, function () {
+        gm(buffer)
+          .roll(shiftX, shiftY)
+          .write(tempImgRoll, function () {
+
+            gm().setFormat(data.format)
+              .in('-page', '+0+0')
+              .in(tempImgRoll)
+              .in('-page', '+' + 0 + '+' + 0)
+              .compose('Add')
+              .in(tempImg)
+              .mosaic()
+              .toBuffer(function (err, buff) {
+                fs.unlinkSync(tempImg);
+                if (err) callback(err);
+                gm(buff)
+                // .crop(imgWidth - shiftX, imgHeight - shiftY, shiftX, shiftY)
+                //  .resize(imgWidth, imgHeight)
+                .toBuffer(function (err, buffy) {
+                  if (err) callback(err);
+                  callback(null, buffy);
+                })
+              });
+          });
+      });
+  });
+};
